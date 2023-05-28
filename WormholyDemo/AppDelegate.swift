de945b4c0b405c54caa793f878551ce52bfecc35
Federico Frappi <federico.frappi@appstyx.com>
2023-05-28 18:15:33 +0200

    Do not start automatically

--- WormholyDemo/AppDelegate.swift
+++ WormholyDemo/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        Wormholy.start()
+
         // Limit the number of API requests stored
         Wormholy.limit = 10
         
