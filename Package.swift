f077fe87af93b45dfcc7a91f22ad2860f69389fd
Matteo Innocenti <matteo@appstyx.com>
2026-09-10 11:35:22 +0200

    Fix SPM support

--- Package.swift
+++ Package.swift
@@ -16,7 +16,16 @@ let package = Package(
         .target(
             name: "WormholySwift",
             dependencies: [],
+            exclude: [
+                "UI/Cells/ActionableTableViewCell.xib",
+                "UI/Cells/RequestCell.xib",
+                "UI/Cells/TextTableViewCell.xib",
+                "UI/Flow.storyboard",
+                "UI/Sections/RequestTitleSectionView.xib"
+            ],
             resources: [
+                .process("Models/Postman/Postman_demo_collection.json"),
+                .process("SPMResources"),
                 .process("Support Files/Assets.xcassets")
             ]
         ),
